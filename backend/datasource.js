3a081a010765afd0b9c593884c6dccc10fa87d66
Christine Zhao <66979909+Penguinfami@users.noreply.github.com>
2024-07-19 19:33:20 -0400

    Update datasource.js

--- backend/datasource.js
+++ backend/datasource.js
@@ -1,3 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL);
+export const sequelize = new Sequelize("db", "user", "password", {
+  host: process.env.DATABASE_URL,
+  dialect: "postgres"
+});
