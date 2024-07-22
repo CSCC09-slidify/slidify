ca54dda9772bb6c484a45e5688e7232b5e931dab
Christine Zhao <66979909+Penguinfami@users.noreply.github.com>
2024-07-22 00:54:38 -0400

    Update socket.service.js

--- frontend/src/services/socket.service.js
+++ frontend/src/services/socket.service.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = new io("http://localhost:3000");
+const socket = new io("https://backend-app-f3w7geeriq-uc.a.run.app");
 
 export const websocket = socket;
