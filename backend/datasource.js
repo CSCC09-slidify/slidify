1539565d05b49c612eced02a78999097c894320a
Christine Zhao <66979909+Penguinfami@users.noreply.github.com>
2024-07-25 18:13:48 -0400

    Update datasource.js

--- backend/datasource.js
+++ backend/datasource.js
@@ -1,3 +1,3 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(process.env.BACKEND_URL ?? "postgres://postgres:password@146.148.63.108/slidify_db?sslmode=disable");
+export const sequelize = new Sequelize(process.env.DATABASE ?? "postgres://postgres:password@146.148.63.108/slidify_db?sslmode=disable");
