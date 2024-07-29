7490ae419b3ae4bafe23ff89e72e9095022e1d07
Penguinfami <christine.spyphone.zhao@gmail.com>
2024-07-29 17:59:32 -0400

    refactor

--- backend/app.js
+++ backend/app.js
@@ -63,8 +63,6 @@ registerIOListeners(io);
 
 app.use((req, res, next) => {
   console.log("HTTP request", req.method, req.url, req.body);
-  console.log(req.session)
-  console.log(req.sessionID)
   req.io = io;
   next();
 });
